77adba6671e0a471ec3ce656ec171517b0c8a261
xiaozhang <1941374675@qq.com>
2020-03-10 23:40:59 +0800

    提交task模块，任务查询界面的代码，和后台代码框架

--- task-management/src/main/java/com/zq/controller/TaskController.java
+++ task-management/src/main/java/com/zq/controller/TaskController.java
@@ -4,13 +4,17 @@ import com.zq.commons.pojo.ResultBean;
 import com.zq.entity.TTask;
 import com.zq.service.ITaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author ZQ
  * @Date 2020/2/29
  */
-@RestController
+@Controller
 @RequestMapping("task")
 public class TaskController {
 
@@ -35,9 +39,12 @@ public class TaskController {
     }
 
     @GetMapping("query/{taskId}")
-    public ResultBean queryTaskByTaskId(@PathVariable("taskId") Long taskId){
-
-        return null;
+    public String queryTaskInfoByTaskId(@PathVariable("taskId") Long taskId, ModelMap modelMap){
+        //查询出taskInfo的list
+        List<TaskInfo> list = taskService.queryTaskInfoByTaskId(taskId);
+        //跳转到任务详情界面
+        modelMap.put("list",list);
+        return "taskinfo";
     }
 
     @GetMapping("queryTask/{userId}")
@@ -46,22 +53,14 @@ public class TaskController {
         return null;
     }
 
-    @GetMapping("queryArchive/{userId}")
-    public ResultBean queryArchiveByUserId(@PathVariable("userId") Long userId){
+    @GetMapping("queryGroupInfoById/{id}")
+    public String queryGroupInfoById(@PathVariable("id") Long groupId, ModelMap modelMap){
+        List<GroupInfo> list = taskService.queryGroupInfoById(groupId);
+        //TODO 查询出groupId下的taskList,并set到GroupInfo对象中
 
-        return null;
-    }
-
-    @GetMapping("queryRecyclebin/{userId}")
-    public ResultBean queryRecyclebinByUserId(@PathVariable("userId") Long userId){
-
-        return null;
-    }
-
-    @GetMapping("queryReport/{userId}")
-    public ResultBean queryReportByUserId(@PathVariable("userId") Long userId){
-
-        return null;
+        //跳转到任务详情界面
+        modelMap.put("list",list);
+        return "groupinfo";
     }
 
     @PostMapping("join/{userId}/{taskId}")
